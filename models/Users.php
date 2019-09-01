5ee1c48790a0cc36f1d8186e47633c53d28b77e6
pushshok <pushshok@gmail.com>
2019-09-01 20:24:23 +0300

    Создание Rbac компонента, контроллера, представлений, миграция и правил

---
+++ models/Users.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $login
+ * @property string $name
+ * @property string $email
+ * @property string $pass
+ * @property string $token
+ * @property string $auth_key
+ * @property string $createAt
+ * @property int $isDeleted
+ *
+ * @property Activity[] $activities
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['email', 'pass'], 'required'],
+            [['createAt'], 'safe'],
+            [['isDeleted'], 'integer'],
+            [['login', 'name', 'email', 'pass', 'token', 'auth_key'], 'string', 'max' => 150],
+            [['email'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'login' => Yii::t('app', 'Login'),
+            'name' => Yii::t('app', 'Name'),
+            'email' => Yii::t('app', 'Email'),
+            'pass' => Yii::t('app', 'Pass'),
+            'token' => Yii::t('app', 'Token'),
+            'auth_key' => Yii::t('app', 'Auth Key'),
+            'createAt' => Yii::t('app', 'Create At'),
+            'isDeleted' => Yii::t('app', 'Is Deleted'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getActivities()
+    {
+        return $this->hasMany(Activity::class, ['user_id' => 'id']);
+    }
+}
